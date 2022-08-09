53aee05c1e660e5df0c6ff67052de0e9fb7bf9a4
Kevin Mas <maskevin27@gmail.com>
2022-08-09 07:01:38 +0200

    update portfolio

---
+++ components/About.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div className="w-full md:h-screen p-2 flex items-center py-16">
+            <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
+                <div className="col-span-2">
+                    <p>About</p>
+                    <h2>Who I Am</h2>
+                    <p>I'm your special web developer</p>
+                    <p>I've spent around 10 years working as a sound engineer, then I came back home where I graduated a
+                        nurse degree and worked as nurse for almost 6 years. I always loved computed and everything
+                        related to the web. In 2020, I started working on my new project, developing a website, working
+                        with HTML, CSS, VueJs & Drupal CMS. Soon I found that I was really fascinated with computer
+                        programming and decided to pass a certification in web development.</p>
+                    <p>I was since my young age really into computers, hardware & software. As soon as I started
+                        programming with HTML & CSS, I was really interested in learning always more. I started working
+                        on Javascript in order to make websites more interactive. After a while, I worked on projects
+                        using VueJS, React JS, NodeJS and MongoDB. I'm planning on learning always more and being able
+                        to fulfill every mission.</p>
+                    <p>Below are some of my latest projects and applications I worked on.</p>
+                </div>
+                <div>
+                    <img
+                        src="https://images.unsplash.com/photo-1537432376769-00f5c2f4c8d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1925&q=80"
+                        alt="/"/>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default About;
+
+// 53:37

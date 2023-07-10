fdfc40da0884169598da6d993e09191e39a4caa8
Kevin Mas <kmas@thess-corp.fr>
2023-07-10 20:50:47 +0200

    feat: adding crwn shop to the website

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,34 +1,61 @@
-import React from 'react';
-import ProjectItem from "./ProjectItem";
+import React from "react";
+import { useTranslation } from "react-i18next";
 import bookiImg from "../public/assets/projects/booki.jpg";
-import ohmyfoodImg from "../public/assets/projects/ohmyfood.jpg"
-import kanapImg from "../public/assets/projects/kanap.jpg"
-import hottakesImg from "../public/assets/projects/hottakes.jpg"
-import groupomaniaImg from "../public/assets/projects/groupomania.jpg"
-import { useTranslation } from 'react-i18next';
+import crwnImg from "../public/assets/projects/crwn.jpg";
+import groupomaniaImg from "../public/assets/projects/groupomania.jpg";
+import hottakesImg from "../public/assets/projects/hottakes.jpg";
+import kanapImg from "../public/assets/projects/kanap.jpg";
+import ohmyfoodImg from "../public/assets/projects/ohmyfood.jpg";
+import ProjectItem from "./ProjectItem";
 
 const Projects = () => {
-    const { t } = useTranslation();
-    return (
-        <div id="projects" className="w-full">
-            <div className="max-w-[1240px] mx-auto px-2 py-16">
-                <p className="text-xl tracking-widest uppercase text-[#1b6dc1]">{t("projects_title")}</p>
-                <h2 className="py-4">{t("projects_subtitle")}</h2>
-                <div className="grid md:grid-cols-2 gap-8">
-                    <ProjectItem title={t("projects_booki_title")} backgroundImg={bookiImg} projectUrl='/booki'
-                                 mainTechnos="HTML 5 / CSS 3" />
-                    <ProjectItem title={t("projects_ohmyfood_title")} backgroundImg={ohmyfoodImg} projectUrl='/ohmyfood'
-                                 mainTechnos="HTML 5 / SASS" />
-                    <ProjectItem title={t("projects_kanap_title")} backgroundImg={kanapImg} projectUrl='/kanap'
-                                 mainTechnos="HTML 5 / CSS 3 / JS" />
-                    <ProjectItem title={t("projects_hottakes_title")} backgroundImg={hottakesImg} projectUrl='/hottakes'
-                                 mainTechnos="NODE.JS / EXPRESS / MONGODB / ANGULAR" />
-                    <ProjectItem title={t("projects_groupomania_title")} backgroundImg={groupomaniaImg}
-                                 projectUrl='/groupomania' mainTechnos="VUE3 / NODE.JS / EXPRESS / MONGODB" />
-                </div>
-            </div>
-        </div>
-    );
+	const { t } = useTranslation();
+	return (
+		<div id="projects" className="w-full">
+			<div className="max-w-[1240px] mx-auto px-2 py-16">
+				<p className="text-xl tracking-widest uppercase text-[#1b6dc1]">{t("projects_title")}</p>
+				<h2 className="py-4">{t("projects_subtitle")}</h2>
+				<div className="grid md:grid-cols-2 gap-8">
+					<ProjectItem
+						title={t("projects_booki_title")}
+						backgroundImg={bookiImg}
+						projectUrl="/booki"
+						mainTechnos="HTML 5 / CSS 3"
+					/>
+					<ProjectItem
+						title={t("projects_ohmyfood_title")}
+						backgroundImg={ohmyfoodImg}
+						projectUrl="/ohmyfood"
+						mainTechnos="HTML 5 / SASS"
+					/>
+					<ProjectItem
+						title={t("projects_kanap_title")}
+						backgroundImg={kanapImg}
+						projectUrl="/kanap"
+						mainTechnos="HTML 5 / CSS 3 / JS"
+					/>
+					<ProjectItem
+						title={t("projects_hottakes_title")}
+						backgroundImg={hottakesImg}
+						projectUrl="/hottakes"
+						mainTechnos="NODE.JS / EXPRESS / MONGODB / ANGULAR"
+					/>
+					<ProjectItem
+						title={t("projects_groupomania_title")}
+						backgroundImg={groupomaniaImg}
+						projectUrl="/groupomania"
+						mainTechnos="VUE3 / NODE.JS / EXPRESS / MONGODB"
+					/>
+					<ProjectItem
+						title={t("projects_crwn_title")}
+						backgroundImg={crwnImg}
+						projectUrl="/crwn"
+						mainTechnos="REACT JS / REDUX / FIREBASE / STRIPE"
+					/>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Projects;
--- i18n/traductions/en.js
+++ i18n/traductions/en.js
@@ -1,60 +1,72 @@
 const en = {
-    translation: {
-        // About component
-        "about_title": "About",
-        "about_who": "Who am I ?",
-        "about_who_description": "I'm your special web developer",
-        "about_who_background": "I've spent around 10 years working as a sound engineer, then I came back home where I graduated a nurse degree and worked as nurse for almost 6 years. I always loved computed and everything related to the web. In 2020, I started working on my new project, developing a website, working with HTML, CSS, VueJs & Drupal CMS. Soon I found that I was really fascinated with computer programming and decided to pass a certification in web development.",
-        "about_who_background_passion": "I was since my young age really into computers, hardware & software. As soon as I started programming with HTML & CSS, I was really interested in learning always more. I started working on Javascript in order to make websites more interactive. After a while, I worked on projects using VueJS, React JS, NodeJS and MongoDB. I'm planning on learning always more and being able to fulfill every mission.",
-        "about_projects": "Below are some of my latest projects and applications I worked on.",            
-        // Contact component
-        "contact_title": "Contact",
-        "contact_subtitle": "Get in touch",
-        "contact_developer": "Full-Stack developer",
-        "contact_developer_description": "I'm available for freelance or full-time positions. Contact me and let's work together.",
-        "contact_connect": "Connect with me",
-        "contact_form_name": "Name",
-        "contact_form_phone": "Phone Number",
-        "contact_form_email": "Email",
-        "contact_form_subject": "Subject",
-        "contact_form_message": "Message",
-        "contact_form_message_button": "Send Message",
-        // Main component
-        "main_pre_title": "Let's build your website or application together",
-        "main_title_presentation": "Hello, I'm",
-        "main_title_subtitle": "A Full-Stack Web Developer",
-        "main_title_description": "I'm a full-stack web developer specializing in building great digital experiences. I'm focused on building responsive web applications with frameworks and libraries.",
-        // Navbar component
-        "navbar_home": "Home",
-        "navbar_about": "About",
-        "navbar_skills": "Skills",
-        "navbar_projects": "Projects",
-        "navbar_mobile_title": "Let's build new Websites and Apps together",
-        "navbar_mobile_connect": "Let's connect",
-        // Project item component
-        "project_item_info": "More Info",
-        // Projects component & pages
-        "projects_title": "Projects",
-        "projects_subtitle": "What I have built",
-        "projects_overview": "Overview",
-        "projects_back": "Back",
-        "projects_booki_title": "Holiday reservation Site",
-        "projects_booki_description": "This app was built using HTML 5 & CSS 3 only. This is an integration from a design which was given. All the app is fully responsive and has different designs on mobile, tablet and desktop. It includes three sections: filters, hostels and activities. Both hostels and activities are displayed as cards.",
-        "projects_ohmyfood_title": "Food reservation site",
-        "projects_ohmyfood_description": "This app was built using HTML5 & SASS for CSS. This is an integration from a design usign animations, on-page loader. The app is fully responsive and has different looks depending on which device you're looking at it. This app is built around 4 pages, and has different features : you can like menus, compose your menu and send your order. This is just a front-end app.",
-        "projects_kanap_title": "Couches commercial app",
-        "projects_kanap_description": "This app was built using mainly JS to dynamically link backend to front-end. The app is a classical commercial app. You can add the product that you have chose to a cart stored in localstorage, so that it persists throughout navigation on site. You can add and remove objects to your cart and when order is passed, you have a unique order number which is sent to you.",
-        "projects_hottakes_title": "Sauce sharing app",
-        "projects_hottakes_description": "This app was build using node JS to create a fully operationnal CRUD api. The app is secured, it respects the standards of OWASP. You can sign up usign email and password and use them as well for log in. Every data stored in the database is secure. Every user can create, update and delete sauces. It's possible to like / dislike sauces too only one like / dislike is allowed per user per sauce.",
-        "projects_groupomania_title": "Company social network app",
-        "projects_groupomania_description": "This app was entirely built from scratch. The backend part is written using NodeJs, Express and MongoDB. Everything is stored in the database in a secured way. The app respects the standards of OWASP. It is possible to sign up using email and password, log in. The rest of the API is a classic CRUD API, every user is able to create, update and delete a post. Each user can like / dislike any post, once per post. The first user created in the database will be an administrator who can moderate the social network (he can update and delete any post).",
-        // Skills component
-        "skills_title": "Skills",
-        "skills_subtitle": "What I can do",
-         // Index page
-         "index_title": "Kevin MAS | Web Developer",
-         "index_name_content": "I'm your personnal web developer. I can create for you personnalized web applications and web sites!",
-    }
-}
+	translation: {
+		// About component
+		about_title: "About",
+		about_who: "Who am I ?",
+		about_who_description: "I'm your special web developer",
+		about_who_background:
+			"I've spent around 10 years working as a sound engineer, then I came back home where I graduated a nurse degree and worked as nurse for almost 6 years. I always loved computed and everything related to the web. In 2020, I started working on my new project, developing a website, working with HTML, CSS, VueJs & Drupal CMS. Soon I found that I was really fascinated with computer programming and decided to pass a certification in web development.",
+		about_who_background_passion:
+			"I was since my young age really into computers, hardware & software. As soon as I started programming with HTML & CSS, I was really interested in learning always more. I started working on Javascript in order to make websites more interactive. After a while, I worked on projects using VueJS, React JS, NodeJS and MongoDB. I'm planning on learning always more and being able to fulfill every mission.",
+		about_projects: "Below are some of my latest projects and applications I worked on.",
+		// Contact component
+		contact_title: "Contact",
+		contact_subtitle: "Get in touch",
+		contact_developer: "Full-Stack developer",
+		contact_developer_description:
+			"I'm available for freelance or full-time positions. Contact me and let's work together.",
+		contact_connect: "Connect with me",
+		contact_form_name: "Name",
+		contact_form_phone: "Phone Number",
+		contact_form_email: "Email",
+		contact_form_subject: "Subject",
+		contact_form_message: "Message",
+		contact_form_message_button: "Send Message",
+		// Main component
+		main_pre_title: "Let's build your website or application together",
+		main_title_presentation: "Hello, I'm",
+		main_title_subtitle: "A Full-Stack Web Developer",
+		main_title_description:
+			"I'm a full-stack web developer specializing in building great digital experiences. I'm focused on building responsive web applications with frameworks and libraries.",
+		// Navbar component
+		navbar_home: "Home",
+		navbar_about: "About",
+		navbar_skills: "Skills",
+		navbar_projects: "Projects",
+		navbar_mobile_title: "Let's build new Websites and Apps together",
+		navbar_mobile_connect: "Let's connect",
+		// Project item component
+		project_item_info: "More Info",
+		// Projects component & pages
+		projects_title: "Projects",
+		projects_subtitle: "What I have built",
+		projects_overview: "Overview",
+		projects_back: "Back",
+		projects_booki_title: "Holiday reservation Site",
+		projects_booki_description:
+			"This app was built using HTML 5 & CSS 3 only. This is an integration from a design which was given. All the app is fully responsive and has different designs on mobile, tablet and desktop. It includes three sections: filters, hostels and activities. Both hostels and activities are displayed as cards.",
+		projects_ohmyfood_title: "Food reservation site",
+		projects_ohmyfood_description:
+			"This app was built using HTML5 & SASS for CSS. This is an integration from a design usign animations, on-page loader. The app is fully responsive and has different looks depending on which device you're looking at it. This app is built around 4 pages, and has different features : you can like menus, compose your menu and send your order. This is just a front-end app.",
+		projects_kanap_title: "Couches commercial app",
+		projects_kanap_description:
+			"This app was built using mainly JS to dynamically link backend to front-end. The app is a classical commercial app. You can add the product that you have chose to a cart stored in localstorage, so that it persists throughout navigation on site. You can add and remove objects to your cart and when order is passed, you have a unique order number which is sent to you.",
+		projects_hottakes_title: "Sauce sharing app",
+		projects_hottakes_description:
+			"This app was build using node JS to create a fully operationnal CRUD api. The app is secured, it respects the standards of OWASP. You can sign up usign email and password and use them as well for log in. Every data stored in the database is secure. Every user can create, update and delete sauces. It's possible to like / dislike sauces too only one like / dislike is allowed per user per sauce.",
+		projects_groupomania_title: "Company social network app",
+		projects_groupomania_description:
+			"This app was entirely built from scratch. The backend part is written using NodeJs, Express and MongoDB. Everything is stored in the database in a secured way. The app respects the standards of OWASP. It is possible to sign up using email and password, log in. The rest of the API is a classic CRUD API, every user is able to create, update and delete a post. Each user can like / dislike any post, once per post. The first user created in the database will be an administrator who can moderate the social network (he can update and delete any post).",
+		projects_crwn_title: "Online clothing store showcase website",
+		projects_crwn_description: `This showcase website was developed based on an extensive ReactJS training. Fully customizable, it is possible to modify the data displayed on the website by modifying the Firebase database. Users can create an account using an email address and password. It is also possible to log in using a Google account.`,
+		// Skills component
+		skills_title: "Skills",
+		skills_subtitle: "What I can do",
+		// Index page
+		index_title: "Kevin MAS | Web Developer",
+		index_name_content:
+			"I'm your personnal web developer. I can create for you personnalized web applications and web sites!",
+	},
+};
 
 module.exports = en;
